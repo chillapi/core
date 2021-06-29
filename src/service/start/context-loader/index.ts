2e7f94dddc3ae0ce219884f4b98934210a3bf204
deroude <rvgabriel@yahoo.com>
2021-06-29 23:07:40 +0300

    fixed context loaders

--- src/service/start/context-loader/index.ts
+++ src/service/start/context-loader/index.ts
@@ -1,8 +1,11 @@
+import { Config } from "@chillapi/api";
+import { BootstrapContextLoader } from "./BootstrapContextLoader";
 import { ContextLoader } from "./ContextLoader";
 import { MethodDelegateContextLoader } from "./MethodDelegateContextLoader";
 import { ModuleContextLoader } from "./ModuleContextLoader";
 
-export const contextLoaders: ContextLoader[] = [
+export const contextLoaders: ContextLoader<Config, any>[] = [
     new ModuleContextLoader(),
+    new BootstrapContextLoader(),
     new MethodDelegateContextLoader()
 ]
