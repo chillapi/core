5f251708135542af9cdc042c36fc5438200b438b
deroude <rvgabriel@yahoo.com>
2021-06-29 14:06:32 +0300

    fixed context loader index

--- src/service/start/context-loader/index.ts
+++ src/service/start/context-loader/index.ts
@@ -1,10 +1,8 @@
-import { BootstrapContextLoader } from "./BootstrapContextLoader";
 import { ContextLoader } from "./ContextLoader";
 import { MethodDelegateContextLoader } from "./MethodDelegateContextLoader";
 import { ModuleContextLoader } from "./ModuleContextLoader";
 
 export const contextLoaders: ContextLoader[] = [
-    new BootstrapContextLoader(),
     new ModuleContextLoader(),
     new MethodDelegateContextLoader()
 ]
