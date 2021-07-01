5da816e7658035b7ee50cc3a5d374ac06be90ded
deroude <rvgabriel@yahoo.com>
2021-07-01 22:07:46 +0300

    tentative mvp

--- src/service/start/StartTools.ts
+++ src/service/start/StartTools.ts
@@ -12,7 +12,8 @@ import { contextLoaders } from './context-loader';
 import { ContextLoader } from './context-loader/ContextLoader';
 import { NoOpContextLoader } from "./context-loader/NoOpContextLoader";
 import { Bootstrap } from "../../model/Bootstrap";
-
+import _ from "lodash";
+import stream from "stream";
 
 const CONFIG_PATTERN = /\.(yml|yaml)$/;
 
@@ -41,6 +42,104 @@ export async function start(basePath: string, host: string, port: number): Promi
         apiSpec: bootstrap.api as any
     }));
 
+    this.app.use(async (err: any, req: any, res: any, next: any) => {
+        console.log(err);
+        // format error
+        res.status(err.status || 500).json({
+            message: err.message,
+            errors: err.errors,
+        });
+    });
+
+    if (!ctx.MethodDelegate) {
+        return Promise.reject('No method delegates found');
+    }
+
+    for (const [apiPath, item] of Object.entries(bootstrap.api.paths)) {
+        const formattedPath = apiPath.replace(/\{(.+?)\}/g, ':$1');
+        const opPath = `${basePath}${formattedPath}`;
+        for (const method of ['get', 'post', 'put', 'delete', 'patch', 'options'].filter(m => _.has(item, m))) {
+
+            const apiConfig = _.get(item, method);
+            const middleware: any[] = [];
+            // if (apiConfig.security) {
+            //     middleware.push(passport.authenticate('jwt', { session: false }));
+            // }
+            // const multipart = apiConfig.requestBody?.content['multipart/form-data'];
+            // if (multipart) {
+            //     const uploadFields = [];
+            //     for (const [propName, propSpec] of Object.entries((multipart.schema as OpenAPIV3.SchemaObject).properties)) {
+            //         if ((propSpec as OpenAPIV3.SchemaObject).type === 'array'
+            //             && ((propSpec as OpenAPIV3.ArraySchemaObject).items as OpenAPIV3.SchemaObject).format === 'binary') {
+            //             uploadFields.push({ name: propName, maxCount: 10 });
+            //         }
+            //         if ((propSpec as OpenAPIV3.SchemaObject).type === 'string'
+            //             && (propSpec as OpenAPIV3.SchemaObject).format === 'binary') {
+            //             uploadFields.push({ name: propName, maxCount: 1 });
+            //         }
+            //     }
+            //     middleware.push(upload.fields(uploadFields));
+            // }
+            this.expressRequest(method, opPath, middleware, async (aReq: any, aRes: any) => {
+                const methodDelegate = Object.values(ctx.MethodDelegate).find(d => d.config.path === apiPath && d.config.method === method)
+                if (!methodDelegate) {
+                    aRes.status(501).send('Method not yet implemented');
+                    return;
+                }
+                try {
+                    const params: any = { ...aReq };
+
+                    if (methodDelegate.transactional) {
+                        this.context.persistence.beginTransaction();
+                    }
+                    for (const delegate of methodDelegate.pipe) {
+                        await delegate.process(this.context, params);
+                    }
+                    if (methodDelegate.transactional) {
+                        this.context.persistence.commitTransaction();
+                    }
+                    if (methodDelegate.returnVar) {
+                        const responseSpec = apiConfig.responses[200]?.content;
+                        if (responseSpec) {
+                            const [contentType, contentSchema] = Object.entries(responseSpec)[0];
+                            if ((contentSchema as any).schema.type === 'string'
+                                && (contentSchema as any).schema.format === 'binary') {
+                                const dl: Buffer = params[methodDelegate.returnVar];
+                                aRes.status(200);
+                                aRes.set({
+                                    'Cache-Control': 'no-cache',
+                                    'Content-Type': contentType,
+                                    'Content-Length': dl.byteLength,
+                                    'Content-Disposition': 'attachment; filename=' + methodDelegate.returnVar
+                                });
+                                const bufferStream = new stream.PassThrough();
+                                bufferStream.end(dl);
+                                bufferStream.pipe(aRes);
+                            } else {
+                                aRes.status(200).send(params[methodDelegate.returnVar]);
+                            }
+                        } else {
+                            aRes.status(200).send(params[methodDelegate.returnVar]);
+                        }
+                    } else {
+                        aRes.status(200).send();
+                    }
+                } catch (err) {
+                    if (methodDelegate.transactional) {
+                        this.context.persistence.rollbackTransaction();
+                    }
+                    console.error(err);
+                    if (err.responseCode) {
+                        aRes.status(err.responseCode).send(err.responseMessage);
+                    } else {
+                        aRes.status(500).send(err);
+                    }
+                }
+
+            });
+        }
+    }
+
     // start the express server
     this.app.listen(port, host, () => {
         // tslint:disable-next-line:no-console
@@ -48,7 +147,7 @@ export async function start(basePath: string, host: string, port: number): Promi
     });
 }
 
-export async function loadConfigurations(basePath: string): Promise<Context> {
+async function loadConfigurations(basePath: string): Promise<Context> {
     const context: Context = {};
     try {
         for await (const f of getConfigFiles(basePath)) {
@@ -69,7 +168,7 @@ export async function loadConfigurations(basePath: string): Promise<Context> {
 }
 
 
-export async function* getConfigFiles(dir: string): AsyncIterableIterator<string> {
+async function* getConfigFiles(dir: string): AsyncIterableIterator<string> {
     const dirents = await readdir(dir, { withFileTypes: true });
     for (const dirent of dirents) {
         const res = resolve(dir, dirent.name);
